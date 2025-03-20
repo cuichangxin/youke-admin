47772b05a12dd7a6474afa1b44d8af38735ec265
cuichangxin <cuichangxin3@163.com>
2025-03-20 20:19:14 +0800

    fix: mock引入

--- mockProdServer.js
+++ mockProdServer.js
@@ -1,4 +1,4 @@
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
+import { createProdMockServer } from 'vite-plugin-mock/client'
 import Mock from './mock'
 export function setupProdMockServer() {
   createProdMockServer([...Mock])
